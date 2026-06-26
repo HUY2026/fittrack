8375acb4152e4787e2690f3229c3d08db92328b6
HUY2026 <reemehr@gmail.com>
2026-06-26 11:20:07 +0700

    Update next.config.js

--- fittrack/next.config.js
+++ fittrack/next.config.js
@@ -2,25 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
       { protocol: 'https', hostname: 'assets.mixkit.co' },
-      { protocol: 'https', hostname: '**.giphy.com' },
-      { protocol: 'https', hostname: 'wger.de' },
       { protocol: 'https', hostname: 'media.giphy.com' },
-      { protocol: 'https', hostname: 'api.exercisedb.io' },
+      { protocol: 'https', hostname: 'wger.de' },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "img-src 'self' data: blob: https://api.exercisedb.io https://assets.mixkit.co https://media.giphy.com https://wger.de; media-src 'self' blob: https://assets.mixkit.co;",
-          },
-        ],
-      },
-    ]
   },
 }
 
